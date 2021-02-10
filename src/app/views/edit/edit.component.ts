d0becd315dfae7fec14566d99b48aa67134bff16
Lucas <lucaszawadneak@gmail.com>
2021-02-10 10:09:54 -0300

    Actual edit page

--- src/app/views/edit/edit.component.ts
+++ src/app/views/edit/edit.component.ts
@@ -9,4 +9,21 @@ export class EditComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  selected = false;
+
+  rating: number = -1;
+  title: string = "";
+  year: number = 0;
+  director: string = "";
+  shootingPrice: number = 0;
+
+  handleRegisterMovie(): void {
+    console.log("filme adicionado");
+
+    this.rating = -1;
+    this.title = "";
+    this.year = 0;
+    this.director = "";
+  }
 }
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -9,9 +9,10 @@ export class HomeComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-  moviePosterUrl =
+  moviePosterUrl: string =
     "https://www.komar.de/en/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/0/2/026-dvd2_star_wars_poster_classic_1_web.jpg";
-  avatarUrl = "https://lucaszawadneak.me/static/media/profile.df0422e2.png";
-  starWarsDescription =
+  avatarUrl: string =
+    "https://lucaszawadneak.me/static/media/profile.df0422e2.png";
+  starWarsDescription: string =
     "Star Wars é uma franquia do tipo space opera estadunidense criada pelo cineasta George Lucas, que conta com uma série de nove filmes de fantasia científica e dois spin-offs.";
 }
